91f5197d08646737ff9f2c3cc177a9024f4c37af
David Villeda <42588903+DavidITT@users.noreply.github.com>
2023-12-21 16:36:10 -0600

    Pages and routing

---
+++ pages/blog/[slug].vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1> Blog: {{ name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "[slug]",
+  setup() {
+    const route = useRoute()
+    const name = route.params.slug
+    return{
+        name
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      All blogs is here
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
